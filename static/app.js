03d258fc124338dcc23ba35514a4b1895de50b00
Agnix <santhoshv9746@gmail.com>
2026-07-10 15:11:41 +0100

    add refresh datad function to fetch and render frontend

--- static/app.js
+++ static/app.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     let tools = [];
     let users = [];
+
+    async function refreshData() {
+        const toolsRes = await fetch('/api/tools');
+        const usersRes = await fetch('/api/users');
+        tools = await toolsRes.json();
+        users = await usersRes.json();
+        
+        renderUsers();
+        renderTools(tools);
+    }
